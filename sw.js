605e6f8f77c11a9d499980e52867907f9567d264
The-Tyler-Labs <thetylerlabs@gmail.com>
2026-04-01 15:38:07 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,30 +1,22 @@
-const CACHE_NAME = 'videotheque-202603312321';
+const CACHE_NAME = 'videotheque-202604011331';
 const FILES_TO_CACHE = ['/MyCinebox/', '/MyCinebox/index.html'];
 
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
-  );
-  // Forcer l'activation immédiate sans attendre
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE)));
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
-  );
-  // Prendre le contrôle immédiatement
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+  ));
   self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
-  // Toujours aller chercher sur le réseau en priorité
   e.respondWith(
     fetch(e.request)
       .then(response => {
-        // Mettre en cache la nouvelle version
         const clone = response.clone();
         caches.open(CACHE_NAME).then(cache => cache.put(e.request, clone));
         return response;
