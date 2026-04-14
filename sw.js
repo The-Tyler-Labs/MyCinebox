8a900f40ac603df81de1c09c5a17753bae9e8a5f
The-Tyler-Labs <thetylerlabs@gmail.com>
2026-04-14 19:05:20 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='videotheque-202604121803';
+const CACHE_NAME='videotheque-202604141658';
 const FILES=['/MyCinebox/','/MyCinebox/index.html'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(FILES)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(n=>n!==CACHE_NAME).map(n=>caches.delete(n)))));self.clients.claim();});
