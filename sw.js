984ad45c9a0cdb877f1f4d961d5f0b685169c10c
The-Tyler-Labs <thetylerlabs@gmail.com>
2026-04-18 11:27:28 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-// 202604171734
+// 202604180916
 self.addEventListener('install',()=>self.skipWaiting());
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.map(n=>caches.delete(n)))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',e=>e.respondWith(fetch(e.request)));
