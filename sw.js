c229102f3c89dfcf65ce7245ae9d40a139387a48
The-Tyler-Labs <thetylerlabs@gmail.com>
2026-07-06 18:33:33 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// MyCinebox service worker — version 2026-07-05-v1.7.20-images-import
-const CACHE_NAME = 'mycinebox-cache-2026-07-05-v1-7-20-images-import';
+// MyCinebox service worker — version 2026-07-05-v1.7.21-images-import
+const CACHE_NAME = 'mycinebox-cache-2026-07-05-v1-7-21-images-import';
 const APP_SHELL = [
   '/MyCinebox/',
   '/MyCinebox/index.html',
