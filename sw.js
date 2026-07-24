e81bdf1b4c211d64ababbbfd0a055d9145dbdb2a
The-Tyler-Labs <thetylerlabs@gmail.com>
2026-07-24 20:48:10 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// MyCinebox service worker — v1.7.43 affichage statuts dans Collection
-const CACHE_NAME = 'mycinebox-v1.7.43';
+// MyCinebox service worker — v1.7.45 correction statistiques paramètres
+const CACHE_NAME = 'mycinebox-v1.7.45';
 const APP_SHELL = [
   '/MyCinebox/',
   '/MyCinebox/index.html',
